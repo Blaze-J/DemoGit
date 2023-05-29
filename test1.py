1975756142e40641172326a154ced4c4114a1a12
Blaze <anhtgcs18069@fpt.edu.vn>
2023-05-29 20:23:32 +0700

    init project

---
+++ test1.py
@@ -0,0 +1,81 @@
+# x = "awesome"
+
+# def myfunc():
+#   x = "fantastic"
+#   print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+# if 5 > 6 :
+#  print("blaze!")
+
+# x = 5
+# y = "An"
+# print(type(x))
+# print(type(y))
+            # import random
+
+            # print(random.randrange(1, 10))
+# a = """Lorem ipsum dolor sit amet,
+# consectetur adipiscing elit,
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+# for x in "banana":
+#   print(x)
+# txt = "The best things in life are free!"
+# # for txt in "The best things in life are free!":
+# print("free" in txt)
+# txt = "The best things in life are free!"
+# if "free" in txt:
+#   print("Yes, 'free' is present.")
+# b = "Hello, World!"
+# print(b[-5:-2])
+# a = "Hello, Horld!"
+# print(a.replace("H", "J"))
+
+
+# x = 1    # int
+# y = 2.8  # float
+# z = 1j   # complex
+
+# #convert from int to float:
+# a = float(x)
+
+# #convert from float to int:
+# b = int(y)
+
+# #convert from int to complex:
+# c = complex(x)
+
+# print(a)
+# print(b)
+# print(c)
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+
+# import random # Ham chon random so
+
+# print(random.randrange(1, 50)) # Ham chon random so
+
+# b = "Blaze, is that you "
+# print(b[0:])
+
+# age = 36
+# txt = "My name is John, and I am {}"
+# # print(txt.format(age))
+# txt = "\x48\x65\x6c\x6c\x6f"
+# print(txt) 
+# a = "We alway call them is \'VIking\' "
+# print(a)
+list1 = ("apple", "banana", "cherry")
+list2 = (1, 5, 7, 9, 3)
+list3 = (True, False, False)
+
+print(list1)
+print(list2)
+print(list3)
